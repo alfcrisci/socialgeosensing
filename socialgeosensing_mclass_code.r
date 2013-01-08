e08f1aac61fdccf24585b052fd0d321e4bbf0e21
Alfonso <alfcrisci@gmail.com>
2013-01-08 23:08:56 +0100

    Update socialgeosensing_mclass_code.r

--- socialgeosensing_mclass_code.r
+++ socialgeosensing_mclass_code.r
@@ -38,14 +38,14 @@ rea_aprile_day=brick("rea_aprile_day.nc")
 # WRF-ARW 9km analisys tmax surface   tempreature (*C)
 # Source: LaMMA-IBIMET CNR 
 
-wrf_tmax_day=brick("data\wrf.tmax.heatwave.nc")
+wrf_tmax_day=brick("wrf.tmax.heatwave.nc")
 
 ##################################################################################
 
 ##################################################################################
 # Calculation and mapping  
 
-# Linear association by R lm function REANALISYS large scale grid 
+# Tree levels of significance in pixelwise linear association between DTKN and weather done by R lm function data : REANALISYS NCEP NOAA (large scale grid) 
 
 cor_map_s=class_sign_regresvec(rea_aprile_day,DKTN_total)
  
@@ -53,11 +53,11 @@ png(file = "REA_sign_lm.png",width = 1024, height = 768, bg = "transparent")
 plot(cor_map_s,xlim = c(-5, 20), ylim = c(35,46),col=brewer.pal(3,"Reds"),legend=F)
 nations_bounds <- getMap(resolution = "low")
 plot(nations_bounds,xlim = c(5, 20), ylim = c(35, 46),asp = 1,add=T)
-legend("bottom", inset=.05, title="Social network Association",c("No","Weak","Strong"), fill=brewer.pal(3,"Reds"), horiz=TRUE)
+legend("bottomleft", inset=.05, title="Social network Association",c("No","Weak","Strong"), fill=brewer.pal(3,"Reds"), horiz=TRUE)
 dev.off()
 
 # Non parametric  association by spearman rank correlation function 
-
+# not perform because not respect linear hypothesis of association between weather and time-aggregated and keytagged social streams
 
 cor_map=stackcortimevec(rea_aprile_day,DKTN_total,method="spearman")
 
@@ -65,10 +65,10 @@ png(file = "REA_sign_lm.png",width = 1024, height = 768, bg = "transparent")
 plot(cor_map,xlim = c(5, 20), ylim = c(30, 46),col=brewer.pal(4,"Reds"),legend=F)
 nations_bounds <- getMap(resolution = "low")
 plot(nations_bounds,xlim = c(5, 20), ylim = c(35, 46),asp = 1,add=T)
-legend("bottom", inset=.0, title="Social network Association",c("No","Weak","Noticeable","Strong"), fill=brewer.pal(4,"Reds"), horiz=TRUE)
+legend("bottomleft", inset=.0, title="Social network Association",c("No","Weak","Noticeable","Strong"), fill=brewer.pal(4,"Reds"), horiz=TRUE)
 dev.off()
 
-# Linear association by R lm function WRF finer scale grid 
+# Tree levels of significance in pixelwise linear association between DTKN and weather done by R lm function Data WRF ARW 9km daily forecasts (finer scale grid) 
 
 
 cor_map_w=class_sign_regresvec(wrf_tmax_day,DKTN_total) 
@@ -77,7 +77,7 @@ png(file = "WRF_sign_lm.png",width = 1024, height = 768, bg = "transparent")
 plot(cor_map_w,xlim = c(0, 20), ylim = c(35, 47),col=brewer.pal(3,"Reds"),legend=F)
 nations_bounds <- getMap(resolution = "low")
 plot(nations_bounds,xlim = c(0, 20), ylim = c(35, 47),asp = 1,add=T)
-legend("bottom", inset=.05, title="Social network Association",c("No","Weak","Strong"), fill=brewer.pal(3,"Reds"), horiz=TRUE)
+legend("bottomleft", inset=.05, title="Social network Association",c("No","Weak","Strong"), fill=brewer.pal(3,"Reds"), horiz=TRUE)
 dev.off()
 
 
